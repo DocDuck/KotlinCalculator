9115d4d4d416f1f5e591c546e90fe144d40a18b2
Alexandr <alex.mitin.psmu@gmail.com>
2020-01-07 16:39:08 +0500

    подключил библиотеку парсер строки, вроде все работает

--- app/src/main/java/com/hackerman/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/hackerman/kotlincalculator/MainActivity.kt
@@ -2,25 +2,17 @@ package com.hackerman.kotlincalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        fun appendExpression(value: String, isNumber: Boolean) {
-            if(isNumber) {
-                calcResult.text = ""
-                calcExpression.append(value)
-            } else {
-                calcExpression.append(calcResult.text)
-                calcExpression.append(value)
-                calcResult.text = ""
-            }
-        }
 
         /**
          * НАВЕШИВАЕМ СЛУШАТЕЛИ
@@ -38,14 +30,49 @@ class MainActivity : AppCompatActivity() {
         calcSeven.setOnClickListener { appendExpression("7", true) }
         calcEight.setOnClickListener { appendExpression("8", true) }
         calcNine.setOnClickListener { appendExpression("9", true) }
+        calcDot.setOnClickListener { appendExpression(".", true) }
         // Выражения
         calcPlus.setOnClickListener { appendExpression("+", false) }
         calcMinus.setOnClickListener { appendExpression("-", false) }
         calcMultiple.setOnClickListener { appendExpression("*", false) }
         calcDiv.setOnClickListener { appendExpression("/", false) }
-        calcRemainder.setOnClickListener { appendExpression("%", false) }
         calcOpen.setOnClickListener { appendExpression("(", false) }
         calcClose.setOnClickListener { appendExpression(")", false) }
+        // Очистить
+        calcClear.setOnClickListener {
+            calcExpression.text = ""
+            calcResult.text = ""
+        }
+        // Убрать последний символ
+        calcBack.setOnClickListener {
+            val stringExpression = calcExpression.text.toString()
+            if(stringExpression.isNotEmpty()){
+                calcExpression.text = stringExpression.substring(0,stringExpression.length-1)
+            }
+            calcResult.text = ""
+        }
+        // При нажатии на равно парсим строку через библиотеку и выдаем цифру
+        calcEqually.setOnClickListener {
+            try {
+                val result = ExpressionBuilder(calcExpression.text.toString()).build().evaluate()
+                calcResult.text = result.toString()
+            } catch(e: Exception) {
+                Log.d("exception", "Что то пошло не так: ${e}, ошибочка")
+            }
+        }
+    }
 
+    private fun appendExpression(value: String, isNumber: Boolean) {
+        if (calcResult.text.isNotEmpty()) {
+            calcResult.text = ""
+        }
+        if(isNumber) {
+            calcResult.text = ""
+            calcExpression.append(value)
+        } else {
+            calcExpression.append(calcResult.text)
+            calcExpression.append(value)
+            calcResult.text = ""
+        }
     }
 }
